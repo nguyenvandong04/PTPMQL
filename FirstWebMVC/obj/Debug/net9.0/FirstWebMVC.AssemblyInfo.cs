d0cf07bb2f943fd1f9bfa62c62348e856d3f1de2
nguyenvandong04 <nguyenvandong041103@gmail.com>
2025-02-12 01:48:56 +0700

    BTW3_CSS

--- FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82dd7bb2e5a83a46a1387d6cb40f1fead67093e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df94988fae69fe907a7934b7a5f741d02be7046a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
