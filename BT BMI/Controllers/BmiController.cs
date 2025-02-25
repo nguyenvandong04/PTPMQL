42ea29ee7b1ac36cad9e689574617f63a4ab14d2
nguyenvandong04 <nguyenvandong041103@gmail.com>
2025-02-25 21:55:15 +0700

    BT_BMI

---
+++ BT BMI/Controllers/BmiController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using BTBMI.Models;
+
+namespace BTBMI.Controllers
+{
+    public class BmiController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Bmi model)
+        {
+            if (model.ChieuCao > 0 && model.CanNang > 0)
+            {
+                float chieuCaoMet = model.ChieuCao / 100; // Chuyển cm → m
+                float bmi = model.CanNang / (chieuCaoMet * chieuCaoMet); // Tính BMI
+
+                ViewBag.BMI = $"Chỉ số BMI của bạn là: {bmi:F2}";
+            }
+            else
+            {
+                ViewBag.BMI = "Vui lòng nhập số hợp lệ!";
+            }
+
+            return View();
+        }
+    }
+}
